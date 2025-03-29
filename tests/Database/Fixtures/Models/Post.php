bb65800ae3c613182175f76ffc9a8b99b8bbaa44
dlinck <dlinck.d@gmail.com>
2025-03-29 19:45:52 +0100

    Set package up for testing

---
+++ tests/Database/Fixtures/Models/Post.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Illuminate\Tests\Database\Fixtures\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    public $table = 'posts';
+}
---
+++ tests/Database/Fixtures/Models/User.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Illuminate\Tests\Database\Fixtures\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    protected $guarded = [];
+}
